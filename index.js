f2f1984f3b5277eb41caba386de7fdf246c37292
Alexandre Berthaud <alexandre@berthaud.me>
2016-08-04 15:05:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+function handleRequest(request, response) {
+  console.log(request.method + " - " + request.url);
+  var body = [];
+  request.on('data', function(chunk) { body.push(chunk); }).on('end', function() {
+    body = Buffer.concat(body).toString();
+    console.log(body);
+  });
+  response.end('Hello.');
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(8080, function() {
+  console.log('Server listening on 8080');
+});
